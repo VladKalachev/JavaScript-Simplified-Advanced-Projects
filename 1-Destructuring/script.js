ecf66a961a1bfa0e132195bcb078216d4338877a
Corvax <vlvete1990@yandex.ru>
2021-09-20 21:31:01 +0300

    chore: added templates for lesson one

--- 1-Destructuring/script.js
+++ 1-Destructuring/script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const array = ["A", "B", "C", "D"]
+const person = {
+    name: "Kyle",
+    age: 25,
+    fovoriteFood: "Rice",
+    address: {
+        street: "123 Main St",
+        ciry: "Somewhere",
+    }
+}
+
+const first = array[0]
+const second = array[1]
+
+console.log(first);
+console.log(second);
