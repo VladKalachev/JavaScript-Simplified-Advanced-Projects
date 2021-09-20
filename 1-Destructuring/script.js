311d254c1606b2e2c4af2f6fbbbbbb8dc034d294
Corvax <vlvete1990@yandex.ru>
2021-09-20 21:37:09 +0300

    chore: array distructuring

--- 1-Destructuring/script.js
+++ 1-Destructuring/script.js
@@ -1,16 +1,25 @@
 const array = ["A", "B", "C", "D"]
 const person = {
-    name: "Kyle",
-    age: 25,
-    fovoriteFood: "Rice",
-    address: {
-        street: "123 Main St",
-        ciry: "Somewhere",
-    }
+  name: "Kyle",
+  age: 25,
+  fovoriteFood: "Rice",
+  address: {
+    street: "123 Main St",
+    ciry: "Somewhere",
+  }
 }
 
-const first = array[0]
-const second = array[1]
+// const [first, second, , f] = array;
+// const first = array[0]
+// const second = array[1]
+// console.log(first);
+// console.log(second);
+// console.log(f);
 
-console.log(first);
-console.log(second);
+function addAndMultiply(a, b) {
+  return [a + b, a * b]
+}
+
+const [sum, product] = addAndMultiply(2, 3)
+console.log(sum);
+console.log(product);
