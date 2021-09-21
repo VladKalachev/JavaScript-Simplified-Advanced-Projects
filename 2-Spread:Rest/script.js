8c7e02f6833b69db0325f787b8054a68c15a277c
Corvax <vlvete1990@yandex.ru>
2021-09-21 11:45:40 +0300

    chore: rest object

--- 2-Spread:Rest/script.js
+++ 2-Spread:Rest/script.js
@@ -1,7 +1,44 @@
-const array = ["A", "B", "C", "D", ["E", "F"]]
+// const divs = [...document.querySelectorAll('div')]
+
+// const array = ["A", "B", "C", "D", ["E", "F"]]
+// const numberArray = [2, 3, 4]
+
+// function sum (numbers) {
+//   return numbers.reduce((sum, numbers) => sum + numbers, 0)
+// }
+
+// console.log(sum([1, 3, 3, 4, 5, 60]));
+
+// function sum(multiplier, ...numbers) {
+//   console.log(multiplier);
+//   console.log(numbers);
+//   return multiplier * numbers.reduce((sum, numbers) => sum + numbers, 0)
+// }
+// sum(...numberArray);
+
+// function concatName(firsName, lastName) {
+//   console.log(firsName + " " + lastName)
+// }
+
+// const names = ["Kyle", "Cook"]
+
+// concatName(...names)
+
+// const newArray = [...numberArray]
+// numberArray.push(5)
+
+// console.log(newArray);
+// console.log(numberArray);
+// divs.map(div => {
+//   console.log(div);
+// })
+
+// const [firs, second, ...rest] = array
+
+// console.log(firs, second, rest)
+
 const person = {
-  name: "Kyle",
-  age: 25,
+  name: "Sally",
   fovoriteFood: "Rice",
   address: {
     street: "123 Main St",
@@ -10,14 +47,19 @@ const person = {
   }
 }
 
-// function sum (numbers) {
-//   return numbers.reduce((sum, numbers) => sum + numbers, 0)
-// }
+// const { name, age, ...rest } = person
 
-// console.log(sum([1, 3, 3, 4, 5, 60]));
+// console.log(name, age, rest);
 
-function sum (multiplier, ...numbers) {
-  return multiplier * numbers.reduce((sum, numbers) => sum + numbers, 0)
+const details = {
+  name: "Kyle",
+  age: 25
 }
 
-console.log(sum(10, 2, 4));
+const person2 = { ...details, ... person }
+
+// person.name = "Sally"
+// console.log(person)
+
+console.log(person2);
+
