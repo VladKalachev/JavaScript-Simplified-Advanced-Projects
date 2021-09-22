314849fe140576b1a585a7b11b6e7d98bc316dd8
Corvax <vlvete1990@yandex.ru>
2021-09-22 12:50:40 +0300

    chore: added lesson 3

---
+++ 3-enhanced-object-literals/script.js
@@ -0,0 +1,13 @@
+const firstName = "Kely"
+const age = 25
+const sayHi = function () {
+  console.log("Hi");
+}
+
+const person = {
+  firstName: firstName,
+  age: age,
+  sayHi: sayHi,
+}
+
+console.log(person);
