00c1f6a1d328bcbcebeed79fa156c4d19beca67e
Corvax <vlvete1990@yandex.ru>
2021-09-24 12:05:28 +0300

    chore: added condition

--- 5-null-coalescing/script.js
+++ 5-null-coalescing/script.js
@@ -1,5 +1,8 @@
-function greet(firstName = "Sally", lastName = "Smith") {
+function greet(firstName = "Sally", lastName) {
+  lastName = lastName ?? "Smith"
   console.log(`${firstName} ${lastName}`);
 }
 
-greet("Kyle", "Cook")
+greet("Kyle")
+
+console.log((undefined ?? false) && (true || false))
