6209196897b170f573632c44df09124d89370beb
Corvax <vlvete1990@yandex.ru>
2021-09-24 12:20:41 +0300

    chore: added fn printHobbyOne

--- 6-optional-chaining/script.js
+++ 6-optional-chaining/script.js
@@ -4,11 +4,31 @@ const kyle = {
   address: {
     street: "124 Main St",
     city: "Somewhere",
-  }
+  },
+  // hobbies: ["Bowling"]
+  // sayHi() {
+  //   console.log("Hi");
+  // },
 }
 
-function printStreet(person) {
-  console.log(person.address.street);
+// function printStreet(person) {
+//   // if(person && person.address) {
+//   //   console.log(person.address.street);
+//   // }
+//   // console.log(person && person.address && person.address.street);
+//   console.log(person?.address?.street)
+// }
+
+// printStreet(kyle)
+
+// function callSayHi(person) {
+//   person?.sayHi?.()
+// }
+
+// callSayHi(kyle)
+
+function printHobbyOne(person) {
+  console.log(person?.hobbies?.[0]);
 }
 
-printStreet(kyle)
+printHobbyOne(kyle)
