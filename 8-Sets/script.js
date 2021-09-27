30738f6ece47acb8362d9eca5c3b91328373a575
Corvax <vlvete1990@yandex.ru>
2021-09-27 12:02:09 +0300

    chore: fn removeDups

--- 8-Sets/script.js
+++ 8-Sets/script.js
@@ -1,8 +1,26 @@
-const uniqueList = [1, 2, 3, 4, 5]
-const newNumber = 4
+// const uniqueList = [1, 2, 3, 4, 5]
+// const newNumber = 4
+// const set = new Set([1, 2, 3, 4]);
 
-if(!uniqueList.includes(newNumber)) {
-  uniqueList.add(newNumber)
+// set.add(5)
+// set.add(4)
+
+// set.delete(1)
+// console.log(set.has(2));
+// set.clear()
+// console.log(set);
+// set.forEach(value => {
+//   console.log(value);
+// })
+
+// if(!uniqueList.includes(newNumber)) {
+//   uniqueList.add(newNumber)
+// }
+
+// console.log(uniqueList);
+
+function removeDups(array) {
+  return [...new Set(array)]
 }
 
-console.log(uniqueList);
+console.log(removeDups([1, 2, 3, 3, 4]));
