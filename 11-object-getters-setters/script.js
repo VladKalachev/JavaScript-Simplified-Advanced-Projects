b8d3a069e4385eeea3f8c2d46ecb5c3318a44679
Corvax <vlvete1990@yandex.ru>
2021-09-30 17:58:08 +0300

    chore: fn get set example

--- 11-object-getters-setters/script.js
+++ 11-object-getters-setters/script.js
@@ -1,6 +1,41 @@
 const person = {
-  firstName: "Kyle",
-  lastName: "Cook",
+  _username: 'Web Dev Simplified',
+  // firstName: "Kyle",
+  // lastName: "Cook",
+  // fullName() {
+  //   return this.firstName + " " + this.lastName
+  // }
+  get username() {
+    return this._username
+  },
+  set username(value) {
+    if (value.length > 5) {
+      this._username = value
+    }
+  }
+  // get fullName() {
+  //   return `${this.firstName} ${this.lastName}`
+  // },
+  // set fullName(value) {
+  //   // [this.firstName, this.lastName] = value.split(' ')
+  //   const nameArray = value.split(' ')
+  //   this.firstName = nameArray[0]
+  //   this.lastName = nameArray[1]
+  // }
 }
 
-console.log(person)
+// person.fullName = "Sally Smith"
+
+person.username = "John11"
+console.log(person.username)
+// console.log(person.fullName)
+
+const person2 = {
+  age: 25,
+  get birtYear() {
+    const date = new Date()
+    return date.getFullYear() - this.age
+  }
+}
+
+console.log(person2.birtYear)
