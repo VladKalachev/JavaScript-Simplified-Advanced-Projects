e02ca04669a33ac465abac7ccc1f0f5547f56e95
Corvax <vlvete1990@yandex.ru>
2021-10-01 11:26:09 +0300

    chore: fn product bind

--- 12-Bind/script.js
+++ 12-Bind/script.js
@@ -1,9 +1,46 @@
-window.name = "Global Name"
+// window.name = "Global Name"
 
-const person = {
-  name: "Kyle",
+// const person = {
+//   name: "Kyle",
+// }
+
+// function printName() {
+//   console.log(this.name);
+// }
+
+// const newPrintName = printName.bind(person)
+// newPrintName()
+
+// function sum(a, b) {
+//   return a + b
+// }
+
+// function sumTwo(b) {
+//   return 2 + b
+// }
+
+// const sumTwo = sum.bind(null, 2)
+
+// console.log(sumTwo(5))
+
+// function sumTwoAndThree() {
+//   return 2 + 3
+// }
+
+// const sumTwoAndThree = sum.bind(null, 2, 3)
+
+// console.log(sumTwoAndThree())
+
+function product(a, b) {
+  return a + b
 }
 
-function printName() {
-  console.log(this.name);
-}
+const numbers = [1, 2, 3, 4, 5]
+
+// const newNumbers = numbers.map(number => {
+//   return product(2, number)
+// })
+
+const newNumbers = numbers.map(product.bind(null, 2))
+
+console.log(newNumbers)
