9cb5ddd635a57eb4adace9af4000e626775d0055
Corvax <vlvete1990@yandex.ru>
2021-10-02 18:03:04 +0300

    chore: apply example

--- 12-Call-Apply/script.js
+++ 12-Call-Apply/script.js
@@ -1,11 +1,29 @@
-window.name = "Global Name"
+// window.name = "Global Name"
 
-const person = {
-  name: "Kyle",
+// const person = {
+//   name: "Kyle",
+// }
+
+// function printName() {
+//   console.log(this.name);
+// }
+
+// // printName.bind(person)()
+// // printName.apply(person)
+// printName.call(person, 2)
+
+// function sum(a, b) {
+//   return a + b
+// }
+
+function sum(...numbers) {
+  return numbers.reduce((count, n) => count + n)
 }
 
-function printName() {
-  console.log(this.name);
-}
+// console.log(sum.bind(null, 2)(3));
 
-printName()
+console.log(sum(1, 2, 3))
+
+const numbersToAdd = [1, 2, 3, 4, 5]
+console.log(sum.call(null, ...numbersToAdd));
+console.log(sum.apply(null, numbersToAdd));
