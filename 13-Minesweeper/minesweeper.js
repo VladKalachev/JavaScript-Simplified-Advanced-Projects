f5db6fdefab150cf87e93dabc7399b927986b728
Corvax <vlvete1990@yandex.ru>
2021-10-04 11:26:38 +0300

    chore: fn getMinePositions

--- 13-Minesweeper/minesweeper.js
+++ 13-Minesweeper/minesweeper.js
@@ -9,6 +9,8 @@ const TILE_STATUSES = {
 
 export function createBoard(boradSize, numberOfMines) {
   const board = []
+  const minePositions = getMinePositions(boradSize, numberOfMines)
+
   for (let x = 0; x < boradSize; x++ ) {
     const row = []
     for (let y = 0; y < boradSize; y++ ) { 
@@ -18,7 +20,14 @@ export function createBoard(boradSize, numberOfMines) {
       const tile = {
         element,
         x,
-        y
+        y,
+        mine: true,
+        get status() {
+          return element.dataset.status
+        },
+        set status(value) {
+          this.element.dataset.status = value
+        },
       }
 
       row.push(tile)
@@ -26,4 +35,28 @@ export function createBoard(boradSize, numberOfMines) {
     board.push(row)
   }
   return board
+}
+
+function getMinePositions(boradSize, numberOfMines) {
+  const positions = []
+
+  while(positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boradSize),
+      y: randomNumber(boradSize),
+    }
+    if (positions.some(positionMatch.bind(null, position))) {
+      positions.push(position)
+    }
+  }
+
+  return positions;
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size)
 }
