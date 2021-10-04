37857d5122340533c49f0434c365c2ecf8a6a618
Corvax <vlvete1990@yandex.ru>
2021-10-04 11:17:42 +0300

    chore: create Element

--- 13-Minesweeper/minesweeper.js
+++ 13-Minesweeper/minesweeper.js
@@ -1,11 +1,22 @@
 // Logic
 
+const TILE_STATUSES = {
+  HIDDEN: "hidden",
+  MINE: "mine",
+  NUMBER: "number",
+  MARKED: "marked" 
+};
+
 export function createBoard(boradSize, numberOfMines) {
   const board = []
   for (let x = 0; x < boradSize; x++ ) {
     const row = []
     for (let y = 0; y < boradSize; y++ ) { 
+      const element = document.createElement('div')
+      element.dataset.status = TILE_STATUSES.HIDDEN
+
       const tile = {
+        element,
         x,
         y
       }
--- 13-Minesweeper/script.js
+++ 13-Minesweeper/script.js
@@ -1,3 +1,12 @@
 import { createBoard } from './minesweeper.js'
 
-console.log(createBoard(2, 3))
+const BOARD_SIZE = 2
+const NUMBER_OF_MINES = 2
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
+const boardElement = document.querySelector('.board')
+board.forEach(row => {
+  row.forEach(tile => {
+    boardElement.append(tile.element)
+  })
+})
