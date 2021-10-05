add819a1de3a68ae49e5c25dccfede0ce8409443
Corvax <vlvete1990@yandex.ru>
2021-10-05 12:41:15 +0300

    chore: added fn checkWin

--- 13-Minesweeper/minesweeper.js
+++ 13-Minesweeper/minesweeper.js
@@ -70,7 +70,11 @@ export function revealTile(board, tile) {
 }
 
 export function checkWin(board) {
-  return true
+  return board.every(row => {
+    return row.every(tile => {
+      return tile.status === TILE_STATUSES.NUMBER || (tile.mine && tile.status === TILE_STATUSES.HIDDEN || tile.status === TILE_STATUSES.MARKED)
+    })
+  })
 } 
 
 export function checkLose(board) {
