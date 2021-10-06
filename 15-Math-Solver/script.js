94b90122329119a92b5d507b48630e3e5bdef3f6
Corvax <vlvete1990@yandex.ru>
2021-10-06 19:28:55 +0300

    chore: init element

--- 15-Math-Solver/script.js
+++ 15-Math-Solver/script.js
@@ -1,1 +1,20 @@
-console.log(111)
+// function parse(equation) {
+//   const step = getNextStep()
+//   const result = solve(step)
+//   replaceNextStep(equation, step, result)
+// }
+
+const inputElement = document.getElementById('equation')
+const outputElement = document.getElementById('results')
+const form = document.getElementById('equation-form')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const result = parse(inputElement.value)
+  outputElement.textContent = result
+})
+
+function parse(equation) {
+  return equation
+}
