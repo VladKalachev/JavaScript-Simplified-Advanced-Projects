752417c102fbbaf6c1ab4bd5891bbbf9ce435168
Corvax <vlvete1990@yandex.ru>
2021-10-11 17:26:54 +0300

    chore: Class Basics

---
+++ 23-Class-Basics/script.js
@@ -0,0 +1,46 @@
+// function Person(name, age) {
+//   this.name = name
+//   this.age = age
+//   // this.printName = function () {
+//   //   console.log(this.name)
+//   // }
+// }
+
+// Person.prototype.printName = function () {
+//   console.log(this.name)
+// }
+
+// Person.prototype.printHi = function () {
+//   console.log("Hi")
+// }
+
+// // console.log(Object.create(null))
+
+// const person = new Person("Kyle", 25)
+// // console.log(person)
+// // console.log(Object.getPrototypeOf(Object.getPrototypeOf(Object.getPrototypeOf(person))));
+// // person.printName()
+
+// const person2 = Object.create(person)
+// // console.log(Object.create(person).name)
+// person2.name = 'John'
+// console.log(person2);
+// console.log(Object.getPrototypeOf(person2) === person);
+// person.printName()
+// person.printHi()
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  printName() {
+    console.log(this.name)
+  }
+}
+
+const person = new Person("Kyle", 25)
+console.log(person)
+
+person.printName()
