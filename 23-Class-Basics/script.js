73f5e434ab61bb4793f40943aba231281ec62d0b
Corvax <vlvete1990@yandex.ru>
2021-10-11 17:43:01 +0300

    chore: static field

--- 23-Class-Basics/script.js
+++ 23-Class-Basics/script.js
@@ -29,18 +29,76 @@
 // person.printName()
 // person.printHi()
 
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   static goodName = "Kyle2"
+//   static printHi() {
+//     console.log("Hi")
+//   }
+
+//   printName() {
+//     console.log(this.name)
+//   }
+// }
+
+// // Person.goodName = "Kyle"
+
+// const person = new Person("Kyle", 25)
+// console.log(Person.goodName)
+
+// person.printName()
+
+// function createUser(email, password, language) {
+//   return {
+//     email,
+//     password,
+//     language,
+//     printPassword() {
+//       console.log(this.password)
+//     }
+//   }
+// }
+
+// console.log(createUser("test@test.com", "password", "English"))
+
+// function User(email, password, language) {
+//   this.email = email;
+//   this.password = password;
+//   this.language = language;
+// }
+
+// User.prototype.printPassword = function() {
+//   console.log(this.password)
+// }
+
+// const user = new User("test@test.com", "password", "English")
+// console.log(user)
+
+class User {
+  constructor(email, password, language) {
+   this.email = email;
+   this.password = password;
+   this.language = language;
   }
 
-  printName() {
-    console.log(this.name)
+  get name() {
+    return this.email.split("@")[0]
+  }
+
+  set name(value) {
+    const [, suffix] = this.email.split("@");
+    this.email = value + suffix
+  }
+
+  printPassword() {
+    console.log(this.password)
   }
 }
 
-const person = new Person("Kyle", 25)
-console.log(person)
-
-person.printName()
+const user = new User("test@test.com", "password", "English")
+user.name = "Kyle"
+console.log(user.name)
