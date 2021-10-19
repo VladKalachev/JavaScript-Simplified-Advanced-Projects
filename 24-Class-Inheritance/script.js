c8f211bd7acb361d605ca88e80c5c1fd34ee2834
Corvax <vlvete1990@yandex.ru>
2021-10-19 18:06:09 +0300

    chore: extends

--- 24-Class-Inheritance/script.js
+++ 24-Class-Inheritance/script.js
@@ -7,3 +7,51 @@ class Animal {
     console.log(`I'am ${this.name}`)
   }
 }
+
+// const anumal = new Animal("Brad")
+// const cat = new Animal("Brad")
+// const shark = new Animal("Brad")
+
+// anumal.speak();
+
+class Dog extends Animal {
+  constructor(name, owner) {
+    super(name)
+    this.owner = owner;
+  }
+
+  speak() {
+    console.log('Bark');
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log('Meow');
+  }
+}
+
+const dog = new Dog('Goga', "Vova");
+const cat = new Cat("Cat");
+console.log(cat.speak());
+dog.speak();
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Janitor extends Person {
+  constructor(name, numberOfMops) {
+    super(name)
+    this.numberOfMops = numberOfMops;
+  }
+
+  clean() {
+    console.log(`Cleaned with ${this.numberOfMops} mops`);
+  }
+}
+
+const janitor = new Janitor("Jerry", 5)
+console.log(janitor.clean());
