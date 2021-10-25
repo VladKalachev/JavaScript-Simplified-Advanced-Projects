aa0de5a6d645f02006fbfda6590942d3ff7f1341
Corvax <vlvete1990@yandex.ru>
2021-10-25 12:14:13 +0300

    chore: add event listener

---
+++ 28-Calculator-Project/script.js
@@ -0,0 +1,33 @@
+import Calculator from './Calculator.js'
+
+// Actions
+// 1. Click a number
+// 2. Click clear button
+// 3. Click delete button
+// 4. Click an operation
+// 5. Click the period button
+// 6. Click equals
+
+const calculator = new Calculator();
+
+document.addEventListener("click", e => {
+  if(e.target.matches("[data-all-clear]")) {
+    calculator.clear();
+  }
+
+  if(e.target.matches("[data-number]")) {
+    calculator.addDigit(e.target.textContent);
+  }
+
+  if(e.target.matches("[data-delete]")) {
+    calculator.removeDigit();
+  }
+
+  if(e.target.matches("[data-operation]")) {
+    calculator.chooseOperation(e.target.textContent);
+  }
+
+  if(e.target.matches("[data-equals]")) {
+    calculator.evaluate();
+  }
+})
