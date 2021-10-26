9b37d520cf6013480c9c3860e432bda14f19cd7f
Corvax <vlvete1990@yandex.ru>
2021-10-26 13:39:41 +0300

    chore: fn addFriend

--- 30-Pure-Functions/script.js
+++ 30-Pure-Functions/script.js
@@ -1,10 +1,34 @@
-const array = [1, 2, 3, 4]
-const TAXES = 1.07
-
-function addElement(a, element) {
-  return [...a, element, TAXES]
+const person = {
+  name: 'Vlad',
+  fruends: ['Peta', 'Masha']
 }
 
-console.log(addElement(array, 5));
+function addFriend(person, friendName) {
+  return {...person, fruends: [...person.fruends, friendName]}
+  //person.fruends.push(friendName)
+}
 
-console.log(array);
+console.log(addFriend(person, "Joey"))
+
+console.log(person)
+
+// const array = [1, 2, 3, 4]
+// const TAXES = 1.07
+
+// function addElement(a, element) {
+//   return [...a, element, TAXES]
+// }
+
+// console.log(addElement(array, 5));
+// console.log(addElement(array, 5));
+// console.log(addElement(array, 5));
+// console.log(addElement(array, 5));
+// console.log(addElement(array, 5));
+
+// console.log(array);
+
+
+// 2 + 4 * 3 - 7
+// 2 + 12 - 7
+// 14 - 7
+// 7
