670e26dee48fe34150289470d1b37d85387e55e7
Corvax <vlvete1990@yandex.ru>
2021-11-02 19:30:33 +0300

    chore: test getLargest

--- 37-Jest-Base/script.test.js
+++ 37-Jest-Base/script.test.js
@@ -1,7 +1,16 @@
-const { sum } = require('./script.js')
+const { sum, getLargest } = require('./script.js')
 
 test('it adds numbers correctly', () => {
     const a = 1
     const b = 2
     expect(sum(a, b)).toBe(3)
+})
+
+test('it returns the largest number in the array', () => {
+    const array = [1, 2, 3, 4]
+    expect(getLargest(array)).toBe(4)
+})
+
+test('it returns null when given an empty array', () => {
+    expect(getLargest([])).toBeNull()
 })
