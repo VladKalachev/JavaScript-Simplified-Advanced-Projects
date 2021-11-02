39b5c7af50177d1ee1b3bb27d7f14811023a6ced
Corvax <vlvete1990@yandex.ru>
2021-11-02 19:38:36 +0300

    chore: toEqual vs toBe

--- 37-Jest-Base/script.test.js
+++ 37-Jest-Base/script.test.js
@@ -12,7 +12,7 @@ describe("#getLargest", () => {
   const global = 'Hi'
   test('it returns the largest number in the array', () => {
     const array = [1, 2, 3, 4]
-    expect(getLargest(array)).toBe(4)
+    expect(getLargest(array)).toEqual(4)
   })
   describe("with an empty array", () => {
     test('it returns null', () => {
@@ -21,3 +21,8 @@ describe("#getLargest", () => {
   })
 })
 
+test('equal vs tobe', () => {
+  const obj = { a: 1, b: 2 }
+  expect(obj).toBe(obj)
+  expect(obj).toEqual({ a: 1, b: 2 })
+})
