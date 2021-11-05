d3af99851c8e44b9df98636b0bab2e220dd247c5
Corvax <vlvete1990@yandex.ru>
2021-11-05 21:44:19 +0300

    chore: added end to end test

--- 28-Calculator-Project/cypress/integration/Calculator.test.js
+++ 28-Calculator-Project/cypress/integration/Calculator.test.js
@@ -1,5 +1,38 @@
 describe('calculator', () => {
-  it("simple test", () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
+  it("correctly handles normal calculations", () => {
+    cy.visit('/')
+    cy.contains('4').click()
+    cy.contains('.').click()
+    cy.contains('1').click()
+    cy.get('.primary-operand').should('have.text', '4.1')
+    cy.contains('+').click()
+    cy.get('.primary-operand').should('have.text', '0')
+    cy.get('.secondary-operand').should('have.text', '4.1')
+    cy.get('.history > [data-operation]').should('have.text', '+')
+    cy.contains('6').click()
+    cy.get('.primary-operand').should('have.text', '6')
+    cy.contains('=').click()
+    cy.get('.primary-operand').should('have.text', '10.1')
+  })
+
+  it('correctly handles all clear', () => {
+    cy.contains('4').click()
+    cy.contains('+').click()
+    cy.contains('6').click()
+    cy.contains('AC').click()
+    cy.get('.primary-operand').should('have.text', '0')
+    cy.get('.secondary-operand').should('have.text', '')
+    cy.get('.history > [data-operation]').should('have.text', '')
+  })
+
+  it('correctly handles delete', () => {
+    cy.contains('4').click()
+    cy.contains('4').click()
+    cy.contains('4').click()
+    cy.contains('DEL').click()
+    cy.get('.primary-operand').should('have.text', '0')
   })
 })
