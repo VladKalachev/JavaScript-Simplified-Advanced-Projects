8655bfad18f18eb7f051bf5644e986edee194a8b
Corvax <vlvete1990@yandex.ru>
2021-11-09 19:24:25 +0300

    chore: added test cases

--- 15-Math-Solver/parse.test.js
+++ 15-Math-Solver/parse.test.js
@@ -36,4 +36,22 @@ describe('#parse', () => {
       expect(parse('(3 + 2) * 4')).toBe(20)
     })
   })
+
+  describe('with very large numbers', () => {
+    test('it returns the correct result in scientific notation', () => {
+      expect(parse('10 ^ 30')).toBe(1e30)
+    })
+  })
+
+  describe('with very small numbers', () => {
+    test('it returns the correct result in scientific notation', () => {
+      expect(parse('10 ^ -30')).toBe(1e-30)
+    })
+  })
+
+  describe('with a malformed equation', () => {
+    test('it returns NaN', () => {
+      expect(parse('abc')).toBeNaN();
+    })
+  })
 })
