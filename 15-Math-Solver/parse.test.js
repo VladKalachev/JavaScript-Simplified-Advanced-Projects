d06b22da4b86735df67ef188133b08aa6882a9f5
Corvax <vlvete1990@yandex.ru>
2021-11-09 19:19:51 +0300

    chore: 100% test jest coverage

--- 15-Math-Solver/parse.test.js
+++ 15-Math-Solver/parse.test.js
@@ -1,7 +1,39 @@
 import parse from './parse.js'
 
 describe('#parse', () => {
-  test('it works', () => {
-    expect(parse('2 + 2')).toBe(4)
+  describe('with addition', () => {
+    test('it returns the correct result', () => {
+      expect(parse('2 + 2')).toBe(4)
+    })
+  })
+  
+  describe('with substraction', () => {
+    test('it returns the correct result', () => {
+      expect(parse('3 - 2')).toBe(1)
+    })
+  })
+
+  describe('with exponents', () => {
+    test('it returns the correct result', () => {
+      expect(parse('3 ^ 2')).toBe(9)
+    })
+  })
+
+  describe('with multiplication', () => {
+    test('it returns the correct result', () => {
+      expect(parse('3 * 2')).toBe(6)
+    })
+  })
+
+  describe('with division', () => {
+    test('it returns the correct result', () => {
+      expect(parse('3 / 2')).toBe(1.5)
+    })
+  })
+
+  describe('with parenthesis', () => {
+    test('it returns the correct result', () => {
+      expect(parse('(3 + 2) * 4')).toBe(20)
+    })
   })
 })
