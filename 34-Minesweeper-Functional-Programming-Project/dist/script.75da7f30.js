62aef1aac37e42b47fe5f3fa4c8ea8c43c93bf5e
Corvax <vlvete1990@yandex.ru>
2021-11-12 12:56:58 +0300

    chore: cypress settings

--- 34-Minesweeper-Functional-Programming-Project/dist/script.75da7f30.js
+++ 34-Minesweeper-Functional-Programming-Project/dist/script.75da7f30.js
@@ -1513,7 +1513,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54450" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64617" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
