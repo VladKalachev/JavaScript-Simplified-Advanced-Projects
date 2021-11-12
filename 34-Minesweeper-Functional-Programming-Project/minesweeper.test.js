c21b7ace68ce301351befaccbd32781d2114a7e5
Corvax <vlvete1990@yandex.ru>
2021-11-12 11:47:20 +0300

    chore: test fn markTile

--- 34-Minesweeper-Functional-Programming-Project/minesweeper.test.js
+++ 34-Minesweeper-Functional-Programming-Project/minesweeper.test.js
@@ -1,4 +1,4 @@
-import { createBoard, TILE_STATUSES } from './minesweeper.js'
+import { createBoard, TILE_STATUSES, markedTilesCount, markTile, revealTile } from './minesweeper.js'
 
 describe("#createBoard", () => {
   test("it creates a valid board", () => {
@@ -16,5 +16,75 @@ describe("#createBoard", () => {
     ]
     const board = createBoard(boadrSize, minePositions)
     expect(board).toEqual(expectedBoard)
+  })
+})
+
+describe("#markedTilesCount", () => {
+  test("with some tiles marked", () => {
+    const board = [
+      [
+        { x: 0, y: 0, status: TILE_STATUSES.MARKED, mine: false },
+        { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true }
+      ],
+      [
+        { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false },
+        { x: 1, y: 1, status: TILE_STATUSES.MARKED, mine: false }
+      ],
+    ]
+    expect(markedTilesCount(board)).toEqual(2)
+  })
+
+  test("with no tiles marked", () => {
+    const board = [
+      [
+        { x: 0, y: 0, status: TILE_STATUSES.HIDDEN, mine: false },
+        { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true }
+      ],
+      [
+        { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false },
+        { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false }
+      ],
+    ]
+    expect(markedTilesCount(board)).toEqual(0)
+  })
+
+  test("with all tiles marked", () => {
+    const board = [
+      [
+        { x: 0, y: 0, status: TILE_STATUSES.MARKED, mine: false },
+        { x: 0, y: 1, status: TILE_STATUSES.MARKED, mine: true }
+      ],
+      [
+        { x: 1, y: 0, status: TILE_STATUSES.MARKED, mine: false },
+        { x: 1, y: 1, status: TILE_STATUSES.MARKED, mine: false }
+      ],
+    ]
+    expect(markedTilesCount(board)).toEqual(4)
+  })
+})
+
+describe("#markTile", () => {
+  test("with a hidden tile it marks it", () => {
+    const board = [
+      [
+        { x: 0, y: 0, status: TILE_STATUSES.HIDDEN, mine: false },
+        { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true }
+      ],
+      [
+        { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false },
+        { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false }
+      ],
+    ]
+    const expectedBoard = [
+      [
+        { x: 0, y: 0, status: TILE_STATUSES.MARKED, mine: false },
+        { x: 0, y: 1, status: TILE_STATUSES.HIDDEN, mine: true }
+      ],
+      [
+        { x: 1, y: 0, status: TILE_STATUSES.HIDDEN, mine: false },
+        { x: 1, y: 1, status: TILE_STATUSES.HIDDEN, mine: false }
+      ],
+    ]
+    expect(markTile(board, { x: 0, y: 0 })).toEqual(expectedBoard)
   })
 })
