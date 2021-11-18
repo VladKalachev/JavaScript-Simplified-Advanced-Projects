97ad9235019f84c5c4f3eb51e5e14dc0c7d25481
Corvax <vlvete1990@yandex.ru>
2021-11-18 21:47:41 +0300

    chore: guard clauses

---
+++ 34-Minesweeper-Functional-Programming-Project/test.js
@@ -0,0 +1,34 @@
+function startTimer(timer) {
+  if(timer.enabled) {
+      timer.time = 0
+      timer.start()
+  }
+}
+
+function startTimer(timer) {
+  if (!timer.enabled) return
+  timer.time = 0
+  timer.start()
+}
+
+function getImsuramceDeductible(insurance) {
+  if (insurance.covered) {
+      if(insurance.majorRepair) {
+          return 500
+      } else if (insurance.mediumRepair) {
+          return 300
+      } else {
+          return 100
+      }
+  } else {
+      return 0
+  }
+}
+
+function getInsuramceDedictible(insurance) {
+  if (!insurance.coverad) return 0
+  if (insurance.majorRepair) return 500
+  if (insurance.mediumRepair) return 300
+
+  return 100
+}
